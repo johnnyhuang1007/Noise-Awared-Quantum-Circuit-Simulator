704be04c2f169d244cfb39e52c0eb6d880da8300
johnnyhuang1007 <johnnyhuang1007@gmail.com>
2024-12-26 04:12:04 +0800

    Bell

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,11 @@ using namespace std;
 int main() {
     Q_state q(2);
     q.print();
-    cout<<"AFTER APPLYING U GATE\n";
-    q.apply_UGate(1, {0,0}, {1.0,0}, {1.0,0}, {0,0});
+
+    q.apply_UGate(0, {1.0/sqrt(2),0}, {1.0/sqrt(2),0}, {1.0/sqrt(2),0}, {-1.0/sqrt(2),0});
     q.print();
-    cout<<"REVERSE\n";
-    q.apply_Control_UGate(0, 1, {0,0}, {1.0,0}, {1.0,0}, {0,0});
-    
+
+    q.apply_Control_UGate(1, 0, {0,0}, {1.0,0}, {1.0,0}, {0,0});
     q.print();
 
     
